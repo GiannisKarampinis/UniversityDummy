b62377e3da812b82aaa22ef82b8e6028597e6112
lab234pc125\user <mai24020@uom.edu.gr>
2023-10-27 17:46:27 +0300

    Changed something in enrollment

--- src/Course.java
+++ src/Course.java
@@ -1,7 +1,8 @@
 
 public class Course {
 	public void enroll() {
-		System.out.println("Actions to enroll a student!"); 
+		System.out.println("Actions to enroll a student!");
+		System.out.println("Some more Actions to enroll a student!");
 	}
-
+	
 }
